d0a0bd80ce0a02ab971e373b7e836a4bff75f1d2
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:17:33 +0200

    insert data

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -56,8 +56,18 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating table: " . $conn->error;
 }
+echo "<br>";
+	
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 $conn->close();
+	
 ?>
 </body>
 </html>
