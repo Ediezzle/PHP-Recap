c05d636a77699ae6fb12a491aafeb66a53d85b87
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 12:43:56 +0200

    create database

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -19,6 +19,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+echo "<br>";
+	
+/* for PHP versions prior to 5.2.9 and 5.3.0, use the following code instead:
+
+// Check connection
+if (mysqli_connect_error()) {
+    die("Database connection failed: " . mysqli_connect_error());
+} */
+	
+//The connection will be closed automatically when the script ends. To close the connection before, use the following: $conn->close();
+	
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
 </body>
 </html>
