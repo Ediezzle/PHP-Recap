edae78f0b1721a3c96bcd4fc8b81b3299d846604
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:09:18 +0200

    create table in the myDB database

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -36,6 +36,26 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating database: " . $conn->error;
 }
+echo "<br>";
+$conn->close();
+	
+$conn = new mysqli($servername, $username, $password, "myDB"); //select this database for use
+	
+// sql code to create table
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 
 $conn->close();
 ?>
