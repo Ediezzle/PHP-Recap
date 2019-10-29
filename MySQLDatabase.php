ad4a3699e0555f9a3be16d71e77da9bae63688d8
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:31:33 +0200

    prepared statements and bound parameters

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -89,10 +89,43 @@ if ($conn->multi_query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+$conn->close();
 
+//PREPARED STATEMENTS AND BOUND PARAMETERS
+	
+// Create connection
+$conn = new mysqli($servername, $username, $password, "myDB");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+// set parameters and execute
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+$firstname = "Julie";
+$lastname = "Dooley";
+$email = "julie@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
 $conn->close();
 ?>
-	
-?>
+
 </body>
 </html>
