499bfcf074117402256d52d49ae0938fcb5fb62f
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:22:22 +0200

    get last id

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -62,7 +62,8 @@ $sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+	$last_id = mysqli_insert_id($conn);
+    echo "New record created successfully. Last inserted ID is: $last_id";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
