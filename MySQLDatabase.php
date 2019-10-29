3c0c73f9a8b3d3a038c8bbf8faaef2afd2d1e081
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:26:16 +0200

    insert multiple values at once

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -68,6 +68,30 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 $conn->close();
+echo "<br>";
+	
+$conn = new mysqli($servername, $username, $password, "myDB");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//insert multiple values at once
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('eddie', 'MAnd', 'edmore.mandikiyana@gmail.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mary', 'Moe', 'mary@example.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
 	
 ?>
 </body>
