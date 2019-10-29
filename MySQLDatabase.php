2443bb909edc2100a93319b94bb5619fb9f49294
Ediezzle <edmore.mandikiyana@gmail.com>
2019-10-29 13:37:49 +0200

    retrieve data from database

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -125,6 +125,31 @@ echo "New records created successfully";
 
 $stmt->close();
 $conn->close();
+	
+//SELECT / RETRIEVE DATA
+	
+// Create connection
+$conn = new mysqli($servername, $username, $password, "myDB");
+	
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
 
 </body>
